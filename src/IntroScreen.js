1296a447ad238478c7fd9e85243aa90845ea1b9a
dakotah jennifer <dakotahjennifer@gmail.com>
2025-09-21 18:01:00 -0400

    Feature: Add DakJen Creative logo to IntroScreen

--- src/IntroScreen.js
+++ src/IntroScreen.js
@@ -13,6 +13,7 @@ function IntroScreen() {
         <button onClick={() => navigate('/login')}>Log In</button>
         <button onClick={() => navigate('/request-account')}>Request an Account</button>
       </div>
+      <img src="/djcreative-logo.png" alt="DakJen Creative Logo" className="intro-logo" />
     </div>
   );
 }
