c4d63adfc36db228c33fd9e49fe9bad23daca712
dakotah jennifer <dakotahjennifer@gmail.com>
2025-09-21 18:21:17 -0400

    Fix: Resolve ESLint import/first error in IntroScreen.js

--- src/IntroScreen.js
+++ src/IntroScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-const DjCreativeLogo = require('./assets/djcreative-logo.png'); // Updated import using require()
 import './IntroScreen.css';
+const DjCreativeLogo = require('./assets/djcreative-logo.png'); // Updated import using require()
 
 function IntroScreen() {
   const navigate = useNavigate();
