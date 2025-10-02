bba648dcf91d87f644a7b2c94149784ce1455ee3
dakotah jennifer <dakotahjennifer@gmail.com>
2025-10-02 19:35:01 -0400

    fix: Use NavLink instead of Link in ReportingSidebar.js

--- src/ReportingSidebar.js
+++ src/ReportingSidebar.js
@@ -13,15 +13,15 @@ function ReportingSidebar() {
         <li><NavLink to="/reporting/monthly-reports">Monthly Reports</NavLink></li>
         <li><NavLink to="/reporting/social-media">Social Media</NavLink></li>
         <li><NavLink to="/reporting/physical-marketing">Physical Marketing</NavLink></li>
-          <Link to="/reporting/operations" className="sidebar-link">
+          <NavLink to="/reporting/operations" className="sidebar-link">
             Operations
-          </Link>
-          <Link to="/reporting/admin-tools" className="sidebar-link">
+          </NavLink>
+          <NavLink to="/reporting/admin-tools" className="sidebar-link">
             Admin Tools
-          </Link>
-          <Link to="/reporting/generated" className="sidebar-link">
+          </NavLink>
+          <NavLink to="/reporting/generated" className="sidebar-link">
             Generated Reports
-          </Link>
+          </NavLink>
       </ul>
       <h3 className="sidebar-title">Operations</h3>
       <ul>
